89aa99207686effd7f27c819e4d7b1a3a1bd5f14
FROM-minyoung <918liter@gmail.com>
2023-01-03 18:36:41 +0900

    Feat: todo 상세 레이아웃 수정

--- front-end/src/components/TodoDetail.tsx
+++ front-end/src/components/TodoDetail.tsx
@@ -17,11 +17,40 @@ export default function TodoDetail({ todoId }: TodoId) {
       .then(({ data }) => setTodoDetail(data.data));
   }, []);
 
+  // 삭제
+  const deleteTodo = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const answer = window.confirm(
+      "삭제하시겠습니까? \n삭제한 할일은 복구할 수 없습니다."
+    );
+    if (answer) {
+      api
+        .delete(`/todos/${e.currentTarget.id}`, {
+          headers: {
+            Authorization: `${token}`,
+          },
+        })
+        .then((res) => alert("삭제되었습니다."));
+    }
+  };
+
   return (
-    <div>
-      <div>{todoDetail?.createdAt}</div>
-      <div>디테일 제목 : {todoDetail?.title}</div>
-      <div>디테일 내용 : {todoDetail?.content}</div>
+    <div className="px-3 py-5">
+      <section className="flex justify-between">
+        <div className="text-[30px]">{todoDetail?.title}</div>
+
+        <div className="flex items-end">
+          <button className="text-sm text-gray-900 mr-3">수정</button>
+          <button
+            id={todoId}
+            onClick={deleteTodo}
+            className="text-sm text-gray-400 mr-3"
+          >
+            삭제
+          </button>
+        </div>
+      </section>
+      <div className="text-xs">{todoDetail?.createdAt.split("T")[0]}</div>
+      <div className="mt-7">{todoDetail?.content}</div>
     </div>
   );
 }
--- front-end/src/pages/TodoList.tsx
+++ front-end/src/pages/TodoList.tsx
@@ -11,8 +11,7 @@ export default function TodoList() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const [todoId, setTodoId] = useState("");
-  const [detailState, setDetailState] = useState(false);
-  const [writeState, setWriteState] = useState(true);
+  const [writeState, setWriteState] = useState(false);
 
   const token = localStorage.getItem("key");
   const navigate = useNavigate();
@@ -27,67 +26,55 @@ export default function TodoList() {
       .then(({ data }) => setTodos(data.data));
   }, []);
 
-  // const movedTodoDetail = (todoId: string) => {
-  //   <TodoDetail todoId={todoId} />;
-  // };
-
   // 로그아웃
   const handleLogout = () => {
-    localStorage.removeItem("key");
-    navigate("/");
-  };
-
-  // 삭제
-
-  const deleteTodo = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const answer = window.confirm(
-      "삭제하시겠습니까? \n삭제한 할일은 복구할 수 없습니다."
-    );
+    const answer = window.confirm("로그아웃 하시겠습니까?");
     if (answer) {
-      api
-        .delete(`/todos/${e.currentTarget.id}`, {
-          headers: {
-            Authorization: `${token}`,
-          },
-        })
-        .then((res) => alert("삭제되었습니다."));
+      localStorage.removeItem("key");
+      navigate("/");
     }
   };
 
   const changeState = (e: React.MouseEvent<HTMLInputElement>) => {
     setTodoId(e.currentTarget.id);
-    setDetailState(!detailState);
   };
-  console.log(todoId);
 
   return (
     <div className="max-w-screen-lg">
-      <div className="flex justify-center">
-        <button onClick={handleLogout}>로그아웃</button>
+      <div className="flex justify-center gap-4 mb-4">
+        <button onClick={handleLogout} className="hover:underline">
+          로그아웃
+        </button>
+        <button
+          onClick={() => setWriteState(!writeState)}
+          className="hover:underline"
+        >
+          글쓰기
+        </button>
       </div>
-      <div className="w-[1024px] h-[700px] flex border border-solid border-gray-700">
-        <div className="w-[300px]">
-          <button onClick={() => setWriteState(!writeState)}>글쓰기</button>
-          <div hidden={writeState}>
+      <div className="w-[800px] h-[500px] flex gap-4 border border-solid border-gray-700">
+        <div className="w-[250px] px-5 py-5 text-center">
+          {writeState ? (
             <WriteToDo />
-          </div>
-          {todos.map((todo: Todo) => {
-            return (
-              <div key={todo.id}>
-                <div id={todo.id} onClick={changeState}>
-                  {todo.title}
-                </div>
-                <button id={todo.id} onClick={deleteTodo}>
-                  삭제
-                </button>
-
-                {/* <div id={todo.id} hidden={detailState}>
-                  <TodoDetail todoId={todoId} />
-                </div> */}
-              </div>
-            );
-          })}
+          ) : (
+            <>
+              <div className="mb-3">할 일</div>
+              {todos.map((todo: Todo) => {
+                return (
+                  <div
+                    key={todo.id}
+                    id={todo.id}
+                    onClick={changeState}
+                    className="flex justify-between hover:underline cursor-pointer mb-3"
+                  >
+                    {todo.title}
+                  </div>
+                );
+              })}
+            </>
+          )}
         </div>
+        <div className="my-5 border-r-[1px] border-black"></div>
         {todoId && (
           <div className="flex-auto">
             <TodoDetail todoId={todoId} />
